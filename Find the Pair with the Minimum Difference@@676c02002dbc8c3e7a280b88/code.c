1f403c3a9e9600aa1054c4dbb52d88b82a3522c8
Jashneer Kaur <jashneerkaur9@gmail.com>
2025-03-30 13:28:52 +0530

    Solution to Find the Pair with the Minimum Difference

--- Find the Pair with the Minimum Difference@@676c02002dbc8c3e7a280b88/code.c
+++ Find the Pair with the Minimum Difference@@676c02002dbc8c3e7a280b88/code.c
@@ -1,27 +1,40 @@
 #include <stdio.h>
 
+void sortArray(int n, int arr[]) {
+    for(int i = 0; i<n-1; i++) {
+        for(int j = 0; i<n-1-i; j++) {
+            if(arr[j] > arr[j+1]) {
+                //swap
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+}
 
-int pairs(int n, int arr[]) {
-    int pair1 = -1, pair2 = -1;
-    int mindiff = 1000;
+int  minDiff_pair(int n, int arr[]) {
+
+    int minDiff = arr[1] - arr[0];
+    int pair1 = arr[0], pair2 = arr[1];
+    sortArray(n, arr);
+
     for(int i = 0; i<n; i++) {
         for(int j = i+1; j<n; j++) {
-                int currdiff = arr[j] - arr[i];
-                if(currdiff < mindiff) {
-                    mindiff = currdiff;
-                    pair1 = arr[i];
-                    pair2 = arr[j];
-                }
+            int diff = arr[j] - arr[i];
+
+            if(diff < minDiff) {
+                minDiff = diff;
+                pair1 = arr[i];
+                pair2 = arr[j];
+            }
         }
     }
     printf("%d %d", pair1, pair2);
-  
-    return -1;
-
 }
 
 int main() {
-    int n;
+    int n; 
     scanf("%d", &n);
 
     int arr[n];
@@ -29,5 +42,9 @@ int main() {
         scanf("%d", &arr[i]);
     }
 
-    pairs(n, arr);
+    minDiff_pair(n, arr);
+
+    return 0;
+
+
 }
